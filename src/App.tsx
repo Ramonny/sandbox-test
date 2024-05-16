8787b957c65758d7fb8cf0302c44eba4f21d7fb1
djowvani <gioanhesini@gmail.com>
2024-05-16 20:23:15 -0300

    added types and organized imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import "./App.css";
 import Calculator from "./components/Calculator";
+
+import "./App.css";
 
 const App = () => {
   return (
--- src/components/Calculator/calculatorMetadata.ts
+++ src/components/Calculator/calculatorMetadata.ts
@@ -1,3 +1,22 @@
+type IPayloadData = {
+  digit: string;
+  operation: string;
+};
+
+export type TCalculatorNumber = string | null | undefined;
+
+export type TCalculatorState = {
+  previousOperand: TCalculatorNumber;
+  operation: TCalculatorNumber;
+  currentOperand: TCalculatorNumber;
+  overwrite: boolean;
+};
+
+export type TCalculatorAction = {
+  type: string;
+  payload?: IPayloadData;
+};
+
 export const ACTIONS = {
   ADD_DIGIT: "add-digit",
   CHOOSE_OPERATION: "choose-operation",
